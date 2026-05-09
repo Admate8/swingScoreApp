eea0aa8e42b4e0bf35bf71edbee18da235ea8354
Adrian Wisnios <adrian9.wisnios@gmail.com>
2026-05-09 21:48:08 +0100

    test: force rebuild

--- R/app_server.R
+++ R/app_server.R
@@ -49,7 +49,8 @@ app_server <- function(input, output, session) {
       dplyr::mutate(contestant = stringr::str_to_title(contestant)) |>
       dplyr::distinct(contestant, wsdc_id, contestant_id) |>
       dplyr::arrange(contestant) |>
-      dplyr::filter(contestant != "")
+      dplyr::filter(contestant != "") |>
+      dplyr::mutate(test = 1)
 
     shinyWidgets::pickerInput(
       inputId    = "select_dancer",
