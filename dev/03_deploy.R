fa0c05d2a3ad7dc789dfc2d0b7935f4d839b80fd
Adrian Wisnios <adrian9.wisnios@gmail.com>
2026-05-09 21:29:42 +0100

    chore: clean up the files

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -9,29 +9,28 @@ devtools::check()
 ## Local, CRAN or Package Manager ----
 ## This will build a tar.gz that can be installed locally,
 ## sent to CRAN, or to a package manager
-devtools::build()
+# devtools::build()
 
 # Deploy to Posit Connect or ShinyApps.io
 # In command line.
-devtools::build()
-golem::add_rstudioconnect_file()
-golem::add_shinyappsio_file()
-golem::add_shinyserver_file()
-golem::add_dockerfile_with_renv()
-golem::add_dockerfile_with_renv_shinyproxy()
-rsconnect::deployApp(
-  appName  = desc::desc_get_field("Package"),
-  appTitle = desc::desc_get_field("Package"),
-  appFiles = c(
-    # Add any additional files unique to your app here.
-    "R/",
-    "inst/",
-    #"data/",
-    "NAMESPACE",
-    "DESCRIPTION",
-    "app.R"
-  ),
-  appId       = rsconnect::deployments(".")$appID,
-  lint        = FALSE,
-  forceUpdate = TRUE
-)
+# golem::add_rstudioconnect_file()
+# golem::add_shinyappsio_file()
+# golem::add_shinyserver_file()
+# golem::add_dockerfile_with_renv(output_dir = "deploy")
+# golem::add_dockerfile_with_renv_shinyproxy()
+# rsconnect::deployApp(
+#   appName  = desc::desc_get_field("Package"),
+#   appTitle = desc::desc_get_field("Package"),
+#   appFiles = c(
+#     # Add any additional files unique to your app here.
+#     "R/",
+#     "inst/",
+#     #"data/",
+#     "NAMESPACE",
+#     "DESCRIPTION",
+#     "app.R"
+#   ),
+#   appId       = rsconnect::deployments(".")$appID,
+#   lint        = FALSE,
+#   forceUpdate = TRUE
+# )
