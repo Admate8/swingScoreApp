160014ae45533d2489cfb6c7fc8159f4919cdcaf
Adrian Wisnios <adrian9.wisnios@gmail.com>
2026-05-11 21:57:37 +0100

    feat: add footer

--- R/ui_footer.R
+++ R/ui_footer.R
@@ -1,4 +1,37 @@
 ui_footer <- tags$div(
   class = "customFooter",
-  "XXX"
+  style = glue::glue("display: flex; justify-content: space-between; color: {col_palette$global$primary_light};"),
+  tags$div(
+    class = "left-text",
+    style = "font-size: 1.2rem;",
+    HTML(paste("&copy", lubridate::year(Sys.Date()), "Adrian Wisnios"))
+  ),
+  tags$div(
+    class = "right-text",
+    htmltools::tagList(
+      tags$a(
+        href = "mailto:omega.dancing.score@gmail.com",
+        icon("square-envelope", style = glue::glue("color: {col_palette$global$primary_light}; font-size: 2rem;")),
+        target = "_blank"
+      ),
+
+      tags$a(
+        href = "https://www.linkedin.com/in/adrian-wisnios-022408215/",
+        icon("linkedin", style = glue::glue("color: {col_palette$global$primary_light}; font-size: 2rem;")),
+        target = "_blank"
+      ),
+
+      tags$a(
+        href = "https://github.com/Admate8/swingScoreApp",
+        icon("square-github", style = glue::glue("color: {col_palette$global$primary_light}; font-size: 2rem;")),
+        target = "_blank"
+      ),
+
+      tags$a(
+        href = "https://www.instagram.com/admate8/",
+        icon("square-instagram", style = glue::glue("color: {col_palette$global$primary_light}; font-size: 2rem;")),
+        target = "_blank"
+      )
+    )
+  )
 )
